f2c0f4da2ed3b600db5d3db6616dd4d7fb741735
imrulhasnat kamal <54498171+imrulhasnatkamal@users.noreply.github.com>
2023-08-20 22:20:52 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// bx slider 
+
+   $('.sliderone').bxSlider({
+    auto: true,
+    autoControls: true,
+    stopAutoOnClick: true,
+    pager: true,
+    slideWidth: 600
+  });
+
+  // bx slider 2
+
+  // $('.sliderone2').bxSlider({
+  //   auto: true,
+  //   autoControls: true,
+  //   stopAutoOnClick: true,
+  //   pager: true,
+  //   slideWidth: 600
+  // });
